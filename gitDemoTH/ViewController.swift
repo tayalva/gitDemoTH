d9d4f966ba9cec1ae3809596e1feda0b60116f57
tayalva <tay.smith@me.com>
2017-03-05 12:48:11 -0800

    dummy user updated

--- gitDemoTH/ViewController.swift
+++ gitDemoTH/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let username = "dummyUser"
+    let username = "dummyUserChanged"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
