3ff7c88cf9174a77042da400635cfea3b3226ea2
taylalva <tay.smith@me.com>
2017-03-05 12:41:54 -0800

    changed background color and added constant

--- gitDemoTH/ViewController.swift
+++ gitDemoTH/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let username = "dummyUser"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
